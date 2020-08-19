1f1a48792246ca2216fd61b1fbf9743db51463fc
mij <chocolatey030@gmail.com>
2020-08-19 02:24:22 +0200

    Added logging.

--- mij.go
+++ mij.go
@@ -10,20 +10,23 @@ type DockerImage struct {
 	Name         string
 	PortExternal int
 	PortInternal int
+	Version      string
 }
 
 func (d *DockerImage) Setup() {
-	cmdString := "docker run -d --rm -p " + strconv.Itoa(d.PortExternal) + ":" + strconv.Itoa(d.PortInternal) + " --name nexus sonatype/nexus3:3.16.1"
+	cmdString := "docker run -d --rm -p " + strconv.Itoa(d.PortExternal) + ":" + strconv.Itoa(d.PortInternal) + " --name " + Name + " " + Name + ":" + Version
 	cmd := exec.Command("bash", "-c", cmdString)
 	stdoutStderr, err := cmd.CombinedOutput()
+	log.Info(stdoutStderr)
 	if err != nil {
 		log.Fatal(err, string(stdoutStderr))
 	}
 }
 
 func (d *DockerImage) Shutdown() {
-	cmd := exec.Command("bash", "-c", "docker stop nexus")
+	cmd := exec.Command("bash", "-c", "docker stop "+Name)
 	stdoutStderr, err := cmd.CombinedOutput()
+	log.Info(stdoutStderr)
 	if err != nil {
 		log.Fatal(err, string(stdoutStderr))
 	}
